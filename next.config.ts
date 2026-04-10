4a8b13cf42bd6bb4f18aa79bd5ce0d50d15d8c59
Ganesh Gasain <ganesh.gusain9@live.com>
2026-04-10 23:43:21 +0530

    First commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  // reactStrictMode: true,
   output: "export",
   images: { unoptimized: true },
   basePath: "/evaluation",
